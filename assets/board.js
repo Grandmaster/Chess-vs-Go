406b9e42ca191f338ec5630abd64c90173f0abf9
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-06-22 05:16:41 -0400

    Added grid to board, accepts number of squares to go in grid

--- assets/board.js
+++ assets/board.js
@@ -1,18 +1,22 @@
 // Code that builds the board the game is played on.
-let context = document.getElementsByTagName("canvas")[0].getContext("2d");
-let a = [100, 200];
-let b = [200, 100];
-let i = 0;
-let j = 0;
-context.strokeStyle = "blue";
-context.lineWidth = 10;
-change = setInterval(() => {
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
-  context.strokeRect(100, 100, a[i], b[i]);
-  if (i == 0) i = 1;
-  else if (i == 1) i = 0;
-  j++;
-  if (j > 5) {
-    clearInterval(change);
+const canvas = document.getElementsByTagName("canvas")[0];
+canvas.width = 500;
+canvas.height = 500;
+
+// Initial data
+let context = canvas.getContext("2d");
+let margin = 50;
+
+// Building the board
+context.strokeStyle = "black";
+context.lineWidth = 1;
+var boxnum = prompt("How many boxes should be in your grid?");
+let boxsize = Math.floor((canvas.width - margin) / boxnum);
+console.log(boxnum);
+console.log(boxsize);
+
+for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
+  for (let x = margin / 2; x <= boxsize * boxnum; x += boxsize) {
+    context.strokeRect(x, y, boxsize, boxsize);
   }
-}, 3000);
+}
