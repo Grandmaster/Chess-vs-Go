c3ca07ba1d3c51ce0ad3c9682093252d953f9797
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-06-24 14:37:09 -0400

    Added grid labels for points at top and bottom of grid

--- assets/board.js
+++ assets/board.js
@@ -1,11 +1,12 @@
 // Code that builds the board the game is played on.
 const canvas = document.getElementsByTagName("canvas")[0];
-canvas.width = 500;
-canvas.height = 500;
+canvas.width = 600;
+canvas.height = 600;
 
 // Initial data
 let context = canvas.getContext("2d");
-let margin = 50;
+let margin = 100;
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // Building the board
 context.strokeStyle = "black";
@@ -14,9 +15,25 @@ var boxnum = prompt("How many boxes should be in your grid?");
 let boxsize = Math.floor((canvas.width - margin) / boxnum);
 console.log(boxnum);
 console.log(boxsize);
+x_counter = 0;
+y_counter = 0;
 
 for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
   for (let x = margin / 2; x <= boxsize * boxnum; x += boxsize) {
     context.strokeRect(x, y, boxsize, boxsize);
+    // Adding grid labels on top and bottom of grid
+    if (y == margin / 2) {
+      context.font = "20px serif";
+      context.fillText(alphabet[x_counter], x, y - margin / 4);
+      x_counter++;
+      console.log(x_counter);
+    } else if (y + boxsize > boxsize * boxnum) {
+      context.font = "20px serif";
+      context.fillText(alphabet[x_counter], x, y + boxsize + margin / 4);
+      x_counter++;
+      console.log(x_counter);
+    } else x_counter = 0;
+    console.log(y);
+    console.log(boxsize * boxnum);
   }
 }
