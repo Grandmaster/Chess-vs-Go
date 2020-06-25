133918076bb4f2e373e4ee4a5454f5d0f13adce9
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-06-25 05:45:55 -0400

    Added labels to left and right of grid

--- assets/board.js
+++ assets/board.js
@@ -26,14 +26,22 @@ for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
       context.font = "20px serif";
       context.fillText(alphabet[x_counter], x, y - margin / 4);
       x_counter++;
-      console.log(x_counter);
     } else if (y + boxsize > boxsize * boxnum) {
       context.font = "20px serif";
       context.fillText(alphabet[x_counter], x, y + boxsize + margin / 4);
       x_counter++;
-      console.log(x_counter);
     } else x_counter = 0;
-    console.log(y);
-    console.log(boxsize * boxnum);
+
+    // Adding grid labels on left and right of grid
+    if (x == margin / 2) {
+      context.font = "20px serif";
+      context.fillText(alphabet[y_counter], x - margin / 4, y);
+      console.log(y_counter);
+    } else if (x + boxsize > boxsize * boxnum) {
+      context.font = "20px serif";
+      context.fillText(alphabet[y_counter], x + boxsize + margin / 4, y);
+      y_counter++;
+      console.log(y_counter);
+    }
   }
 }
