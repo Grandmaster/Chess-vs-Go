981fb93d1ca2bc48252e7932ce2c913d22b38961
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-07-03 00:59:32 -0400

    Added code that detects clicks on individual points on game board

--- assets/js/board.js
+++ assets/js/board.js
@@ -17,23 +17,26 @@ console.log(boxnum);
 console.log(boxsize);
 x_counter = 0;
 y_counter = 0;
+gridpoints_array = [];
+gridpoints_labels = [];
 
 for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
   for (let x = margin / 2; x <= boxsize * boxnum; x += boxsize) {
     context.strokeRect(x, y, boxsize, boxsize);
+    gridpoints_array.push([x, y]);
+    gridpoints_labels.push([alphabet[x_counter], alphabet[y_counter]]);
     // Adding grid point labels on top and bottom of grid
     if (y == margin / 2) {
       context.font = "20px serif";
       context.fillText(alphabet[x_counter], x - 5, y - margin / 4);
-      x_counter++;
     } else if (y + boxsize > boxsize * boxnum) {
       context.font = "20px serif";
       context.fillText(alphabet[x_counter], x - 5, y + boxsize + margin / 4);
-      x_counter++;
-    } else x_counter = 0;
+    }
 
     // Adding grid point labels on left and right of grid
     if (x == margin / 2) {
+      x_counter = 0;
       context.font = "20px serif";
       context.fillText(alphabet[y_counter], x - margin / 4, y + 5);
     } else if (x + boxsize > boxsize * boxnum) {
@@ -41,5 +44,10 @@ for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
       context.fillText(alphabet[y_counter], x + boxsize + margin / 4, y + 5);
       y_counter++;
     }
+
+    x_counter++;
   }
 }
+
+console.log(gridpoints_array);
+console.log(gridpoints_labels);
--- assets/js/boardclick.js
+++ assets/js/boardclick.js
@@ -1,6 +1,23 @@
 // Code that handles the click events that happen on the board.
 // ================================================================
 canvas.addEventListener("click", (event) => {
-  console.log("Clicked on the canvas!");
-  console.log(event);
+  console.log([
+    event.clientX - canvas.offsetLeft,
+    event.clientY - canvas.offsetTop,
+  ]);
+  for (let point of gridpoints_array) {
+    if (
+      point[0] - 10 <= event.clientX - canvas.offsetLeft &&
+      event.clientX - canvas.offsetLeft <= point[0] + 10
+    ) {
+      if (
+        point[1] - 10 <= event.clientY - canvas.offsetTop &&
+        event.clientY - canvas.offsetTop <= point[1] + 10
+      ) {
+        index = gridpoints_array.indexOf(point);
+        labels = gridpoints_labels[index];
+        console.log(`You clicked on point (${labels[0]},${labels[1]})`);
+      }
+    }
+  }
 });
