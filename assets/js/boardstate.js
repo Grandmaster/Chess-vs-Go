b3964f37391ef38e8e69681de51b3cefd064f7a4
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-09-10 06:19:56 -0400

    Added code that builds game state for entire game from game states for go and chess seperately

--- assets/js/boardstate.js
+++ assets/js/boardstate.js
@@ -3,13 +3,36 @@
 
 $(document).ready(() => {
   console.log("The window is ready!");
-  // Total game state as an array
-  let elementnum = 2 * parseInt(boxnum) + 1;
-  console.log(elementnum);
-  let gamestate = [];
-  let row = new Array(elementnum).fill(0);
-  for (let i = 0; i < elementnum; i++) {
-    gamestate.push(row);
+  // Converting user input to a number
+  num_box = parseInt(boxnum);
+
+  // Go game state as an array
+  let pointnum = num_box + 1;
+  let gostate = [];
+  let go_row = new Array(pointnum).fill(0);
+  for (let i = 0; i < pointnum; i++) {
+    gostate.push(go_row);
   }
-  console.log(gamestate);
+
+  // Chess game state as an array
+  let gridnum = num_box;
+  let chessstate = [];
+  let chess_row = new Array(gridnum).fill(0);
+  for (let i = 0; i < gridnum; i++) {
+    chessstate.push(chess_row);
+  }
+
+  // Function to build total game state from chess and go game states
+  function TotalGameState(go_state, chess_state) {
+    let gamestate = [];
+    for (let i = 0; i <= pointnum - 1; i++) {
+      gamestate.push(go_state[i]);
+      if (i <= chess_state.length - 1) {
+        gamestate.push(chess_state[i]);
+      }
+    }
+    return gamestate;
+  }
+
+  let computedGameState = TotalGameState(gostate, chessstate);
 });
