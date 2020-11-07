9cdc7f5138f0fb2a0ae436a2e8dd56f0d246e206
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-11-06 20:21:24 -0500

    Created go_state.js in front end, to keep track of go portion of game state

---
+++ assets/js/boardstate/go_state.js
@@ -0,0 +1,5 @@
+// Code that keeps track of the state of the go/baduk/weiqi portion of the game.
+// ==================================================================================
+$(document).ready(() => {
+  console.log(godash);
+});
