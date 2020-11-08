01762195df02e9323f72749e0e14187541c63370
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-11-08 05:13:45 -0500

    Added drawn image of pawn to assets; game displays pawn on board after each click; other chess pieces will be added after being made in Krita

--- assets/js/boardclick.js
+++ assets/js/boardclick.js
@@ -11,14 +11,10 @@ $(document).ready(() => {
   // Black goes first
   color = godash.BLACK;
 
-  // Object that stores chess pieces that can be used on Canvas. Pieces retrieved from Font Awesome.
+  // Object that stores chess pieces that can be used on Canvas, by storing the url of the image;
+  // Pieces made in Krita.
   const chess_pieces = {
-    king: "\uf43f",
-    queen: "\uf445",
-    pawn: "\uf443",
-    knight: "\uf441",
-    rook: "\uf447",
-    bishop: "\uf43a",
+    pawn: "./images/Pawn.png",
   };
 
   // Object that keeps track of go/weiqi/baduk state of game, courtesy of godash
@@ -57,6 +53,12 @@ $(document).ready(() => {
     go_board = godash.addMove(go_board, point, color);
     if (x_true != -1 && y_true != -1) {
       currentGoBoard(go_board, ctx, canvas_go.width, canvas_go.height, boxsize);
+      var img = new Image();
+      img.onload = () => {
+        ctx.drawImage(img, 350, 350, 70, 70);
+      };
+      img.src = chess_pieces.pawn;
+      console.log(img);
     }
     color = godash.oppositeColor(color);
   });
