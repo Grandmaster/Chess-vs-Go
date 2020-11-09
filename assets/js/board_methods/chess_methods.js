15aeabaa86750500141de4f200e7b6bf86a2f19f
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-11-09 03:51:20 -0500

    Improved file structure

--- assets/js/board_methods/chess_methods.js
+++ assets/js/board_methods/chess_methods.js
@@ -1,2 +1,8 @@
 // Code for various manipulations of the chess portion of the game
 // =========================================================================
+
+// Object that stores chess pieces that can be used on Canvas, by storing the url of the image;
+// Pieces made in Krita.
+const chess_pieces = {
+  pawn: "./images/Pawn.png",
+};
