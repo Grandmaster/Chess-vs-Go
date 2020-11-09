87b7b6e1987eca93cc6a915504d7cdd4bfdbba3f
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-11-09 05:57:22 -0500

    Added file that will handle click events involving both aspects of the game

---
+++ assets/js/board_click/hybrid_click.js
@@ -0,0 +1,9 @@
+// Code that handles click events concerning both chess and go portions of the game.
+// ====================================================================================
+
+// Parent div
+const div = document.getElementsByTagName("div")[0];
+
+$(document).ready(() => {
+  // Event listener for clicks on the board
+});
