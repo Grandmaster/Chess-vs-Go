51c0daa5bfd998827a7e31cec6232d286261b24d
Chidi <59777043+Grandmaster@users.noreply.github.com>
2020-11-10 05:17:45 -0500

    Click events on the chess layer now trigger click events on the lower go layer

--- assets/js/board_click/chess_click.js
+++ assets/js/board_click/chess_click.js
@@ -10,6 +10,15 @@ var contxt = canvas_chess.getContext("2d");
 $(document).ready(() => {
   // Event listener for clicks on the board
   canvas_chess.addEventListener("click", (event) => {
+    // Since this is the top layer, pass click event to go_layer with mouse location
+    var clickEvent = new MouseEvent("click", {
+      clientX: event.clientX,
+      clientY: event.clientY,
+    });
+    canvas_go.dispatchEvent(clickEvent);
+    console.log(event);
+
+    // Finding where on the board the click happened
     let x_point = event.clientX - canvas.offsetLeft;
     let y_point = Math.floor(
       event.clientY - canvas.offsetTop + window.pageYOffset
--- assets/js/board_click/hybrid_click.js
+++ assets/js/board_click/hybrid_click.js
@@ -1,9 +1,10 @@
 // Code that handles click events concerning both chess and go portions of the game.
 // ====================================================================================
 
-// Parent div
-const div = document.getElementsByTagName("div")[0];
-
 $(document).ready(() => {
+  // All layers of the canvas in one variable
+  var layers = $("div").children();
+
   // Event listener for clicks on the board
+  $("div").click((event) => {});
 });
