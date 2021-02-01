c1afda5494b1cb49b18bd8ea8dd219240fda3b6d
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-02-01 01:21:11 -0500

    Removed hybrid click file, because there is no need for it as it does not reference a unique canvas context

--- assets/js/board_methods/hybrid_methods.js
+++ assets/js/board_methods/hybrid_methods.js
@@ -6,9 +6,6 @@
 // - It is occupied by a player's stone, or
 // - The point is empty, but falls under territory (go) controlled by the player
 
-// For the point aspect,
-// - If it is part of the eye of a living group, and there are no enemy stones in the eye, then it should kill the king
-// - If same as above, but there are enemy stones in the eye, the king should live, until the stones are cleared out
-// - If it is part of the eye of a dying group, but there are no stones within, the king should be killed
-// - If same as above, but enemy stones are present, the king should live
-// - For all other situations involving killing the king via territory, the king should live
+// In this game, pawns can kill stones by capturing them, which moves the pawn to the square diagonal to
+// its previous square, connected by the stone
+function pawnCapturesStone() {}
