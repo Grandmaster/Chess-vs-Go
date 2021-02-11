16332652ef0c9e06fdcd2f15b3750f72731c98e0
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-02-10 21:37:14 -0500

    Still working on implementing tiling functionality

--- assets/js/board_methods/hybrid_methods.js
+++ assets/js/board_methods/hybrid_methods.js
@@ -7,7 +7,7 @@
 // - The point is empty, but falls under territory (go) controlled by the player
 
 // Data for creating player zones via tiling, courtesy of TilertheCreator
-import { TilerTheCreator } from "../../../TilerTheCreator/TilerTheCreator.js";
+// import { TilerTheCreator } from "../../../TilerTheCreator/TilerTheCreator.js";
 
 // Boolean that determines whether there is an opportunity for a pawn to capture a stone
 var stonesCanBeCaptured = false;
