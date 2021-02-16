58ece77997348e64b66fca603cb38f7ebb4e6e88
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-02-16 01:28:37 -0500

    Pieces can now only be summoned onto zones controlled by the player

--- assets/js/board_click/chess_click.js
+++ assets/js/board_click/chess_click.js
@@ -130,14 +130,16 @@ $(document).ready(() => {
       // Place piece if the first two cases don't apply, and place_queue is not empty
       if (c == 0 && place_queue.length !== 0) {
         var piece = place_queue.pop();
-        switch (color) {
-          case "black":
+        let land = displayZones(goBoardforChess, field, contxt)[color];
+        let check = false;
+        for (let sq of land) {
+          if (sameSquare(sq, [x_i, y_i])) {
             placePiece(x_i, y_i, piece.img, piece.type);
+            check = true;
             break;
-
-          case "white":
-            placePiece(x_i, y_i, piece.img, piece.type);
+          }
         }
+        if (!check) console.log("Not a valid landing zone");
         currentChessBoard(
           pieces_in_play,
           contxt,
