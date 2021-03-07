796a1de930824108d365a26fc5e9bba9413a825e
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-03-06 20:38:08 -0500

    App now saves user tagname to localStorage and directs them to lobby

--- assets/js/setup/home.js
+++ assets/js/setup/home.js
@@ -1,2 +1,12 @@
 // Javascript file to handle user interaction with home page
 // ====================================================================
+
+// Redirecting the user to the lobby with their entered tagname
+let form = document.getElementsByTagName("form")[0];
+console.log(form);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let tagname = document.getElementById("gamename").value;
+  localStorage.setItem("tag", tagname);
+  window.open("/lobby.html", "_self");
+});
