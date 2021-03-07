c1fa0bac4e733cb742705fd4a4d9eb087f000a6e
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-03-06 22:27:19 -0500

    Worked a bit on lobby.html

--- assets/js/setup/lobby.js
+++ assets/js/setup/lobby.js
@@ -4,5 +4,8 @@
 // Getting the tagname from localStorage
 var tagname = localStorage.getItem("tag");
 if (tagname !== null) {
-  console.log(`Welcome back, ${tagname}!`);
+  let intro = document.getElementById("intro");
+  let introText = intro.innerHTML;
+  let introWithName = introText.replace("!", `, ${tagname}!`);
+  intro.innerHTML = introWithName;
 }
