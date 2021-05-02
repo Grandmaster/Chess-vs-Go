a9fde065463fdb35d3615c903cb37deb60fa25a6
Chidi <59777043+Grandmaster@users.noreply.github.com>
2021-05-02 01:09:34 -0400

    Rewrote board building code; board is now built on html5 canvas using canvas pattern API

--- assets/js/game/board.js
+++ assets/js/game/board.js
@@ -14,71 +14,33 @@ if (gamename !== null) {
 // Initial data
 let context = canvas.getContext("2d");
 let margin = 150;
-let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// Building the board
-context.strokeStyle = "black";
-context.lineWidth = 1;
 var boxnum = 8;
 var boxsize = (canvas.width - margin) / boxnum;
-let x_counter = 0;
-let y_counter = 0;
-let y_count_prev = y_counter;
-let alt_counter = 0;
-let gridpoints_array = [];
-let gridpoints_labels = [];
+let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-// Function to help change color of squares
-function flip(counter, val1, val2) {
-  if (counter == val1) {
-    return val2;
-  } else if (counter == val2) {
-    return val1;
-  }
-}
+// Create pattern
+let pcan = document.createElement("canvas");
+let pcon = pcan.getContext("2d");
+pcan.width = (canvas.width - margin) / (boxnum / 2);
+pcan.height = pcan.width;
 
-for (let y = margin / 2; y <= boxsize * boxnum; y += boxsize) {
-  for (let x = margin / 2; x <= boxsize * boxnum; x += boxsize) {
-    if (y_count_prev !== y_counter) {
-      alt_counter = flip(alt_counter, 0, 1);
-      y_count_prev = y_counter;
-    }
-    if (alt_counter == 0) {
-      context.strokeRect(x, y, boxsize, boxsize);
-      alt_counter = flip(alt_counter, 0, 1);
-    } else if (alt_counter == 1) {
-      context.fillStyle = "rgba(139,69,19,0.5)";
-      context.fillRect(x, y, boxsize, boxsize);
-      context.strokeRect(x, y, boxsize, boxsize);
-      alt_counter = flip(alt_counter, 0, 1);
-    }
-    gridpoints_array.push([x, y]);
-    gridpoints_labels.push([alphabet[x_counter], alphabet[y_counter]]);
-    context.fillStyle = "black";
+// Design pattern
+pcon.strokeStyle = "black";
+pcon.lineWidth = 1;
+pcon.strokeRect(0, 0, pcan.width / 2, pcan.height / 2);
+pcon.strokeRect(0, pcan.height / 2, pcan.width / 2, pcan.height);
+pcon.strokeRect(pcan.width / 2, 0, pcan.width / 2, pcan.height / 2);
+pcon.strokeRect(pcan.width / 2, pcan.height / 2, pcan.width / 2, pcan.height);
+pcon.fillStyle = "rgba(139,69,19,0.5)";
+pcon.fillRect(pcan.width / 2, 0, pcan.width / 2, pcan.height / 2);
+pcon.fillRect(0, pcan.height / 2, pcan.width / 2, pcan.height);
 
-    // Adding grid point labels on top and bottom of grid
-    if (y == margin / 2) {
-      context.font = "20px serif";
-      context.fillText(alphabet[x_counter], x - 5, y - margin / 4);
-    } else if (y + boxsize > boxsize * boxnum) {
-      context.font = "20px serif";
-      context.fillText(alphabet[x_counter], x - 5, y + boxsize + margin / 4);
-      gridpoints_array.push([x, y + boxsize]);
-    }
-
-    // Adding grid point labels on left and right of grid
-    if (x == margin / 2) {
-      x_counter = 0;
-      context.font = "20px serif";
-      context.fillText(alphabet[y_counter], x - margin / 4, y + 5);
-    } else if (x + boxsize > boxsize * boxnum) {
-      context.font = "20px serif";
-      context.fillText(alphabet[y_counter], x + boxsize + margin / 4, y + 5);
-      gridpoints_array.push([x + boxsize, y]);
-      y_count_prev = y_counter;
-      y_counter++;
-    }
-
-    x_counter++;
-  }
-}
+// Print pattern on board
+let pattern = context.createPattern(pcan, "repeat");
+context.fillStyle = pattern;
+context.fillRect(
+  margin / 2,
+  margin / 2,
+  canvas.width - margin,
+  canvas.height - margin
+);
